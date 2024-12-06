ef1f3eb693e105cc8fc1d40b55c91b6ce1287eb8
jokudev <git@joku.dev>
2024-12-06 08:21:38 +0100

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'modul335.demo',
+  appName: 'modul335-demo',
+  webDir: 'www'
+};
+
+export default config;
