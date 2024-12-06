869537dbf0390bc9e5a82d8bf6d10cd797f66e57
jokudev <git@joku.dev>
2024-12-06 14:09:03 +0100

    add supabase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://olevixbouryqwnlyjudy.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sZXZpeGJvdXJ5cXdubHlqdWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0ODg5MTQsImV4cCI6MjA0OTA2NDkxNH0.yL41jgk3mL_66HOwu_T-BhUJf9BbCmhkfzxJlhgOD_o',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://olevixbouryqwnlyjudy.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sZXZpeGJvdXJ5cXdubHlqdWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0ODg5MTQsImV4cCI6MjA0OTA2NDkxNH0.yL41jgk3mL_66HOwu_T-BhUJf9BbCmhkfzxJlhgOD_o',
 };
 
 /*
