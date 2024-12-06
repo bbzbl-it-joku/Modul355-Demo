64066672aceb37e9972df76b408803e66559ec92
jokudev <git@joku.dev>
2024-12-06 11:32:16 +0100

    get emulator to work

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'modul335.demo',
-  appName: 'modul335-demo',
-  webDir: 'www'
-};
-
-export default config;
